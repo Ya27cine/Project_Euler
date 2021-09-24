d8a0a06856c0d43bea8d52738de678dc7c91d061
KHELIFA Yassine <prostam27@gmail.com>
2021-09-25 00:12:53 +0200

    Problem 9

--- script.js
+++ script.js
@@ -1,29 +1,34 @@
 var result = document.getElementById("res") ;
 /**
- * Problem 8
+ * Problem 9
  * 
- *    Find the thirteen adjacent digits in the 1000-digit number that have the greatest product. What is the value of this product? Answer: 
- */
+ * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+ * Find the product abc. 
+ * 
+*/
 
+const limit = 1000;
 
-
-let my_num = "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
-
-
-let limit = my_num.length;
-let thirteen_adjacent = 13;
 function euler(){
-    let sum = 1;
-    for (let i = 0; i < limit; i++) {
-        let test = 1;
-        for(let j=0; j < thirteen_adjacent; j++)
-        // // Find the 13 adjacent digits in the my_num number that have the greatest product
-            test *= my_num.charAt(j+i);
-        if(test> sum)
-        // take the greatest product
-                sum = test;
+    let res = 0;
+    for (let a = 1; a < limit; a++) {
+        for (let b = a; b < limit; b++) {
+            for (let c = b; c < limit; c++) {
+                if( isPythagorean(a, b, c) )
+                    if(a+b+c == 1000){
+                        // one Pythagorean triplet for which a + b + c = 1000.
+                        res = a*b*c;
+                    }
+            }
+        }
     }
-    return sum;
+    return res;
 }
 
 result.innerText = euler();
+
+
+function isPythagorean(a, b, c){
+    return
+         (Math.pow(a,2) + Math.pow(b,2) == Math.pow(c,2) )? true : false;
+}
